b0c764e56471cca25b5025e4ac5fc54f9a3d3c84
Dhamotharan S <dhamotharand347@gmail.com>
2023-09-27 05:41:11 +0530

    Initial commit
    With all the necessary dependencies. Empty version of the project

---
+++ src/main/java/com/xmplar/music/playlist/MusicPlaylistManagerApplication.java
@@ -0,0 +1,13 @@
+package com.xmplar.music.playlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicPlaylistManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicPlaylistManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xmplar/music/playlist/MusicPlaylistManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xmplar.music.playlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicPlaylistManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
